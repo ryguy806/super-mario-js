e4bae2b8f08b4f29e774dd1c597f199f8a540790
ryguy806 <L1ghtSab3r11!!>
2020-08-07 08:36:24 -0700

    Finished fixing mario mechanics

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,7 @@ import Camera from './modules/Camera.js';
 import Timer from './modules/Timer.js';
 import {loadLevel} from './libraries/loaders.js';
 import {createMario} from './libraries/entities.js';
-import {createCollisionLayer, createCameraLayer} from './libraries/layers.js';
 import {setupKeyboard} from './libraries/input.js';
-import {setupMouseControl} from './libraries/debug.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
@@ -19,22 +17,18 @@ Promise.all([
 
     mario.pos.set(64, 64);
 
-    /*level.comp.layers.push(
-        createCollisionLayer(level),
-        createCameraLayer(camera));*/
-
-
     level.entities.add(mario);
 
     const input = setupKeyboard(mario);
     input.listenTo(window);
 
-    setupMouseControl(canvas, mario, camera);
-
-
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime) {
         level.update(deltaTime);
+
+        if(mario.pos.x > 100) {
+            camera.pos.x = mario.pos.x - 100;
+        }
 
         level.comp.draw(context, camera);
     }
