4ac471631b90b9f74dea295763f604277679d8ce
ryguy806 <L1ghtSab3r11!!>
2020-08-10 13:10:53 -0700

    Added enemy entities to the game synchronously.

--- public/js/libraries/entities.js
+++ public/js/libraries/entities.js
@@ -1,47 +1,14 @@
-import Entity from '../modules/Entity.js';
-import Go from '../modules/traits/Go.js';
-import Jump from '../modules/traits/Jump.js';
-import {loadSpriteSheet} from './loaders.js';
-import {createAnim} from './animations.js';
+import {loadMario} from './entities/Mario.js';
+import {loadGoomba} from './entities/Goomba.js';
+import {loadKoopa} from './entities/Koopa.js';
 
-const FAST_DRAG = 1/5000;
-const SLOW_DRAG = 1/1000;
-
-export function createMario() {
-    return loadSpriteSheet('mario')
-    .then(sprite => {
-        const mario = new Entity();
-        mario.size.set(14, 16);
-
-        mario.addTrait(new Go());
-        mario.go.dragFactor = SLOW_DRAG;
-        
-        mario.addTrait(new Jump());
-
-        mario.turbo = function setTurboState(turboOn) {
-            this.go.dragFactor = turboOn ? FAST_DRAG : SLOW_DRAG;
-        }
-
-        const runAnim = createAnim(['run-1', 'run-2', 'run-3'], 6);
-        function routeFrame(mario) {
-            if(mario.jump.falling){
-                 return 'jump';
-            }
-            if (mario.go.distance > 0) {
-                if((mario.vel.x > 0 && mario.go.dir < 0) || (mario.vel.x < 0 && mario.go.dir > 0)) {
-                    return "break";
-                }
-
-                return runAnim(mario.go.distance);
-            }
-
-            return 'idle';
-        }
-
-        mario.draw = function drawMario(context) {
-            sprite.draw(routeFrame(this), context, 0, 0, this.go.heading < 0);
-        }
-
-        return mario;
-    });
+export function loadEntities() {
+    const entitiesFactory = {};
+    
+    return Promise.all([
+        loadMario().then(factory => entitiesFactory['mario'] = factory),
+        loadGoomba().then(factory => entitiesFactory['goomba'] = factory),
+        loadKoopa().then(factory => entitiesFactory['koopa'] = factory),
+    ])
+    .then(() => entitiesFactory);
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,37 @@
 import Camera from './modules/Camera.js';
 import Timer from './modules/Timer.js';
 import {loadLevel} from './libraries/loaders/level.js';
-import {createMario} from './libraries/entities.js';
 import {setupKeyboard} from './libraries/input.js';
+import {createCollisionLayer} from './libraries/layers.js';
+import { loadEntities } from './libraries/entities.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 Promise.all([
-    createMario(),
     loadLevel('1-1'),
+    loadEntities(),
 ])
-.then(([mario, level]) => {
+.then(([level, factory]) => {
+
+    console.log(factory);
     const camera = new Camera();
     window.camera = camera;
 
+    const mario = factory.mario();
     mario.pos.set(64, 64);
+    
+    const goomba = factory.goomba();
+    goomba.pos.x = 220;
+    level.entities.add(goomba);
+
+    const koopa = factory.koopa();
+    koopa.pos.x = 260;
+    level.entities.add(goomba);
 
     level.entities.add(mario);
+
+    level.comp.layers.push(createCollisionLayer(level));
 
     const input = setupKeyboard(mario);
     input.listenTo(window);
